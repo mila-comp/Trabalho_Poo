daaf0d6b896f4e57456be43de1ca55463472e5e6
Kamilah Santos <kamilahsantossouza@gmail.com>
2025-05-21 10:28:20 -0300

    Update events.java

--- fazer/events/events.java
+++ fazer/events/events.java
@@ -2,4 +2,9 @@ package events;
 
 public interface events {
     //vai obrigar o usuário a colocar o evento que quer (ex.: aniversário, data, mes, ano)
+    String getEvento(); //fala o nome do evento
+    int getDia(); //qual dia vai ser
+    int getMes(); //qual mes vai ser
+    int getAno(); //qual ano vai ser
+
 }
