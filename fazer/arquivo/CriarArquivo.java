654b717655966cd104119ca6998c43ee2b203c87
Kamilah Santos <kamilahsantossouza@gmail.com>
2025-05-21 09:59:46 -0300

    Update CriarArquivo.java

--- fazer/arquivo/CriarArquivo.java
+++ fazer/arquivo/CriarArquivo.java
@@ -1,22 +1,44 @@
 package arquivo;
 
-import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class CriarArquivo {
-    public void texto(String filePath) {
+    public void criar(){
         Scanner entrada = new Scanner(System.in);
-        int ans;
+        String texto;
+
 
         System.out.println("\nDigite:");
-
-        ans = entrada.nextInt();
+        texto = entrada.nextLine(); //só para teste, pode ser mudado
         entrada.nextLine(); // Limpa o buffer (ENTER)
 
+        try{
+            FileWriter arq = new FileWriter("testeCalendario.txt", true); //Modo append (true) pra adicionar ao final do arquivo
+            PrintWriter gravarArq = new PrintWriter(arq);
+            gravarArq.println(texto); //escreve a linha do texto no arquivo
+            gravarArq.close(); //fecha o arquivo para garantir a gravação
+            System.out.println("Texto gravado com sucesso!");
+        } catch(
+                IOException e)
+
+        {
+            System.err.format("Erro de E/S: %s%n", e);
+        }
+
+        LimparArquivo.limparArquivo();
+
+        System.out.println("Encerrando o programa...");
 
 
+        entrada.close(); // Fecha o Scanner
+
+    }
+
+}
+/* OUTRA OPÇÃO *
         System.out.println("Digite o texto que deseja gravar no arquivo:"); //vai ser usado LER NO TERMINAL
         String texto = entrada.nextLine(); //Lê o texto digitado no terminal
 
@@ -36,11 +58,11 @@ public class CriarArquivo {
             writer.newLine();
 
             gerarMediaMensal(writer);
-            writer.newLine();*/
+            writer.newLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        /*
+
         try {
             FileWriter arq = new FileWriter("teste.txt", true); //Modo append (true) pra adicionar ao final do arquivo
             PrintWriter gravarArq = new PrintWriter(arq);
@@ -58,5 +80,3 @@ public class CriarArquivo {
 
         entrada.close(); // Fecha o Scanner
         */
-    }
-}
